63e578291f700504edfb27e746fe9b286eb25f8d
staraneelpatra <staraneel@live.com>
2024-02-27 22:50:48 +0530

    Template Driven Form Reset and Prefill

--- AngularApp16/src/app/app.component.ts
+++ AngularApp16/src/app/app.component.ts
@@ -8,12 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  ngModeldata = "";
+   users={
+  name: "Anil",
+  email: "Anil@Patra.ku",
+  mobile: 9937421341
+ };
   formSubmit(val : any){
     console.log(val);
     var name = val.yourname;
     var email = val.email;
     var mobile = val.mobile
     console.log(name, email, mobile);
+    console.log("#%#%#%#%#%#%#^^^^^^^^^^^^^^^^^^^^")
+    console.log(this.users.name);
+    console.log(this.users.email);
   }
 }
